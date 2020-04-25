26ea2c89743786d83fca31418388360422bbd75b
Andrew Rubcov <winchesterpro5@gmail.com>
2020-04-25 21:51:51 +0300

    It's really working! Beta 1.0.0!

--- utils/users.js
+++ utils/users.js
@@ -3,17 +3,22 @@ const users = [];
 // Присоединение пользователя к чату
 function userJoin(id, username, room) {
   const user = { id, username, room };
+  console.log('Изначальный массив: ' + users.length);
   console.log(`Пуш-коннект нью юзера: ${user.username}`);
   if(users.length) {
     let filterUsers = users.find(item =>
       (item.username === user.username) && (item.room === user.room)
     );
-    console.log(filterUsers);
-    if(filterUsers === undefined) return 1;
+    console.log('filterUsers: ' + filterUsers);
+    if(filterUsers === undefined) {
+      users.push(user);
+      return user;
+    }
   } else {
     users.push(user);
     console.log('First push > ' + users[0].username);
   }
+  console.log(user);
   return user;
 }
 
